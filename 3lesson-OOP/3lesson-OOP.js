c269b50fe72de181292f436cd145b919ff7aaf86
kubista9 <jakubkuka07@gmail.com>
2024-02-29 11:26:52 +0100

    Removed script.js and updated 3lesson-OOP.js

--- 3lesson-OOP/3lesson-OOP.js
+++ 3lesson-OOP/3lesson-OOP.js
@@ -2,37 +2,37 @@
 
 
 //1. Using Composition
-// function createCircle(center, radius){
-//     return {
-//         getCenterX: () => center.getX(),
-//         getCenterY: () => center.getY(),
-//         getRadius: () => radius,
-//         moveCenterTo: (x, y) => center.moveTo(x, y),
-//         toString: () =>  `Circle(Center: ${center.toString()}, Radius: ${radius} )`
-//     }
-// }
+function createCircle(center, radius){
+    return {
+        getCenterX: () => center.getX(),
+        getCenterY: () => center.getY(),
+        getRadius: () => radius,
+        moveCenterTo: (x, y) => center.moveTo(x, y),
+        toString: () =>  `Circle(Center: ${center.toString()}, Radius: ${radius} )`
+    }
+}
 
-// function createPoint(x, y) {
-//    return {
-//     // getX: () => x
-//         getX(){
-//             return x
-//         }, 
-//         getY(){
-//             return y
-//         },
-//         moveTo(newX, newY){
-//             x = newX
-//             y = newY
-//         },
-//         //toString: () => '[${x},${y}]'
-//         toString(){
-//         return `[${x},${y}]`; 
-//         }
-//     }
-// }
+function createPoint(x, y) {
+   return {
+    // getX: () => x
+        getX(){
+            return x
+        }, 
+        getY(){
+            return y
+        },
+        moveTo(newX, newY){
+            x = newX
+            y = newY
+        },
+        //toString: () => '[${x},${y}]'
+        toString(){
+        return `[${x},${y}]`; 
+        }
+    }
+}
 
-// console.log(createCircle(createPoint(1, 2), 4).toString())
+console.log(createCircle(createPoint(1, 2), 4).toString())
 
 
 //2. Combining Programming Styles
