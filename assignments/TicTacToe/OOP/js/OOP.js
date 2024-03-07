cc46c6406272bd7d086208a039952242023b964d
Jakub Kuka <jakubkuka07@gmail.com>
2024-03-07 11:41:33 +0100

    adding images

--- assignments/TicTacToe/OOP/js/OOP.js
+++ assignments/TicTacToe/OOP/js/OOP.js
@@ -1,27 +1,34 @@
 // script.js
 
+//getting the variables right for my cells, reset button and I need to mark the turn
 const cells = document.querySelectorAll('[data-cell]');
 const resetButton = document.getElementById('reset-button');
 let turn = 'X';
 
+//once: true - making sure that the cell can be clicked only once 
+//on click event calling the onClick function
 cells.forEach(cell => {
-    cell.addEventListener('click', handleClick, { once: true });
+    cell.addEventListener('click', onClick, { once: true });
 });
 
 resetButton.addEventListener('click', resetGame);
 
-function handleClick(e) {
+//
+function onClick(e) {
+    //e.target - stores the clicked element
     const cell = e.target;
     placeMark(cell, turn);
     if (checkWin(turn)) {
         alert(`${turn} wins!`);
         resetGame();
+        //if the game is not won by the current move, the turn is switched to the other player
     } else {
         turn = turn === 'X' ? 'O' : 'X';
     }
 }
 
 function placeMark(cell, mark) {
+    //classList - provides acess to the list of CSS classes currently applied to the element
     cell.classList.add(mark);
 }
 
@@ -37,15 +44,17 @@ function checkWin(mark) {
         [2, 4, 6]
     ];
 
+    //tests whether at least one element of the array fits into the winning combinations
     return winningCombinations.some(combination =>
         combination.every(index => cells[index].classList.contains(mark))
     );
 }
 
+
 function resetGame() {
     turn = 'X';
     cells.forEach(cell => {
         cell.classList.remove('X', 'O');
-        cell.addEventListener('click', handleClick, { once: true });
+        cell.addEventListener('click', onClick, { once: true });
     });
 }
