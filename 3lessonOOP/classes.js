05773220eec811a33cdaef8c4ae814465b265520
Jakub Kuka <jakubkuka07@gmail.com>
2024-03-14 19:31:10 +0100

    adding examples for lesson 03

---
+++ 3lessonOOP/classes.js
@@ -0,0 +1,16 @@
+
+// Classes
+class Dog {
+    constructor(name, age) {
+        this.name = name
+        this.age = age
+    }
+
+    bark() {
+        return "WUFF"
+    }
+}
+
+const dog = new Dog("Sam", 2)
+
+console.log(dog.bark()) // → WUFF
