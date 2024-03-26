9a7972fad1170b9aea4537ff52585174e7df0198
Jakub Kuka <jakubkuka07@gmail.com>
2024-03-26 20:59:14 +0100

    getting names of all pokemons

--- assignments/Pokedex/src/App.js
+++ assignments/Pokedex/src/App.js
@@ -1,7 +1,28 @@
+import React, { useState, useEffect } from 'react';
+
 function App() {
-  return (
-    <h2>Hello world</h2>
-  )
+ const [pokemons, setPokemons] = useState([]);
+
+ useEffect(() => {
+    const fetchPokemons = async () => {
+      const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=151');
+      const data = await response.json();
+      setPokemons(data.results);
+    };
+
+    fetchPokemons();
+ }, []);
+
+ return (
+    <div>
+      {pokemons.map((pokemon, index) => (
+        <div key={index}>
+          <img src={pokemon.url} alt={pokemon.name} />
+          <p>{pokemon.name}</p>
+        </div>
+      ))}
+    </div>
+ );
 }
 
 export default App;
