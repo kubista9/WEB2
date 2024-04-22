9cfeb53a7c2c5a49f970c71cdd74e2bec84b12d1
Jakub Kuka <jakubkuka07@gmail.com>
2024-04-22 09:45:31 +0200

    shifting the unicode value by 1

--- 9lessonTesting/exercises/Ex6-Caesar Cipher/caesarCipher.js
+++ 9lessonTesting/exercises/Ex6-Caesar Cipher/caesarCipher.js
@@ -1,13 +1,18 @@
 // Ex6 - Caesar Cipher 
 
-function caesarCipher(message, factor){
+/* function caesarCipher(message, factor){
+    
+}  */
 
-    function lettersToNumbers(message) {
-        return message.toLowerCase().split('').map(char => {
-            if (char >= 'a' && char <= 'z') {
-                return char.charCodeAt(0) - 97;
-            }
-            return -1;
-        });
-    }
+let message = "abcd";
+encryptedMessage = '';
+let factor = 1;
+k = factor % 26;
+
+for (let i = 0; i < message.length; i++) {
+    let encryptedChar = message.charCodeAt(i) + 1;
+    let decrypteddChar = String.fromCharCode(encryptedChar);
+    encryptedMessage += decrypteddChar;
 }
+
+console.log(encryptedMessage)
