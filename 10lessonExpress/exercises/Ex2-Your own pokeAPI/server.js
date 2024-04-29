52abf384c2139ca9442ac2812c5151098613a7a8
Jakub Kuka <jakubkuka07@gmail.com>
2024-04-29 10:34:25 +0200

    Ex 2 - your own PokeAPI

---
+++ 10lessonExpress/exercises/Ex2-Your own pokeAPI/server.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import { POKEMON } from './Pokemon.js';
+
+const app = express();
+
+app.get("/pokemon/:id", (req, res) => {
+    const reqId = parseInt(req.params.id)
+    const pokemon = POKEMON.find(pokemon => pokemon.id === reqId)
+    if (pokemon) res.send(pokemon)
+    else res.status(404).send({ msg: "No pokemon with the id of " + reqId })
+  })
+  
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
