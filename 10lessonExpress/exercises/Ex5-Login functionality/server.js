e95510eb3e19d1c3cea636ba36d118f738364126
Jakub Kuka <jakubkuka07@gmail.com>
2024-04-29 20:27:50 +0200

    Copying the solution

--- 10lessonExpress/exercises/Ex5-Login functionality/server.js
+++ 10lessonExpress/exercises/Ex5-Login functionality/server.js
@@ -1,32 +1,52 @@
-import express from 'express';
-import jwt from 'jsonwebtoken';
-import bcrypt from 'bcrypt';
-import dotenv from 'dotenv';
+import express from "express"
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
+import dotenv from "dotenv/config"
 
-dotenv.config();
+const app = express()
+app.use(express.json())
 
-const app = express();
-const PORT = 3000;
-app.use(express.json());
+const users = []
 
-app.post('/login', (req, res) => {
-    const { email, password } = req.body;
-
-    const user = {
-        id: 1,
-        email: 'example@example.com',
-        hashedPassword: '123',
-    };
-
-    bcrypt.compare(password, user.hashedPassword, (err, result) => {
-        if (err || !result) {
-            res.status(401).send('Invalid email or password');
-        } 
-        const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, { expiresIn: '1h' });
-        res.set('Authorization', `Bearer ${token}`).sendStatus(200);
-    });
-}); 
-
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+app.post("/login", async (req, res) => {
+    const { username, password } = req.body
+    try {
+        const user = await authenticateUser(username, password)
+        const token = jwt.sign(user, process.env.JWT_SECRET, { expiresIn: "1h" })
+        res.set("Authorization", `Bearer ${token}`).sendStatus(200)
+    } catch (err) {
+        res.status(401).send({ error: "Unauthorized" })
+    }
 })
+
+async function authenticateUser(username, password) {
+    const user = users.find(user => user.username === username)
+    if (!user) throw new Error("Invalid credentials")
+
+    const passwordMatch = await bcrypt.compare(password, user.password)
+    if (!passwordMatch) throw new Error("Invalid credentials")
+
+    return { username: user.username }
+}
+
+app.post("/signup", async (req, res) => {
+    const { username, password } = req.body
+    try {
+        await createUser(username, password)
+        res.send({ message: "User created!" })
+    } catch (err) {
+        res.status(500).send({ error: "Error signing up" })
+    }
+})
+
+async function createUser(username, password) {
+    const existingUser = users.find(user => user.username === username)
+    if (existingUser) throw new Error("Name taken!")
+
+    const hashedPassword = await bcrypt.hash(password, 10)
+    const newUser = { username, password: hashedPassword }
+    users.push(newUser)
+    return newUser
+}
+
+app.listen(3000)
