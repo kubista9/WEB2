9b363a585f9d510fef2f39543334fa66b626d04e
Jakub Kuka <jakubkuka07@gmail.com>
2024-04-29 20:48:32 +0200

    Ex7-Consuming API from frontend

---
+++ 10lessonExpress/exercises/Ex7-Consuming API from frontend/server.js
@@ -0,0 +1,54 @@
+import express from "express"
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
+import dotenv from "dotenv/config"
+import cors from "cors"
+
+app.use(cors({ exposedHeaders: "Authorization" }))
+const app = express()
+app.use(express.json())
+
+const users = []
+
+app.post("/login", async (req, res) => {
+    const { username, password } = req.body
+    try {
+        const user = await authenticateUser(username, password)
+        const token = jwt.sign(user, process.env.JWT_SECRET, { expiresIn: "1h" })
+        res.set("Authorization", `Bearer ${token}`).sendStatus(200)
+    } catch (err) {
+        res.status(401).send({ error: "Unauthorized" })
+    }
+})
+
+async function authenticateUser(username, password) {
+    const user = users.find(user => user.username === username)
+    if (!user) throw new Error("Invalid credentials")
+
+    const passwordMatch = await bcrypt.compare(password, user.password)
+    if (!passwordMatch) throw new Error("Invalid credentials")
+
+    return { username: user.username }
+}
+
+app.post("/signup", async (req, res) => {
+    const { username, password } = req.body
+    try {
+        await createUser(username, password)
+        res.send({ message: "User created!" })
+    } catch (err) {
+        res.status(500).send({ error: "Error signing up" })
+    }
+})
+
+async function createUser(username, password) {
+    const existingUser = users.find(user => user.username === username)
+    if (existingUser) throw new Error("Name taken!")
+
+    const hashedPassword = await bcrypt.hash(password, 10)
+    const newUser = { username, password: hashedPassword }
+    users.push(newUser)
+    return newUser
+}
+
+app.listen(3000)
